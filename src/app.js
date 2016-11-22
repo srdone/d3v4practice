36e7b1a41a0bbb4d6d422df7dec24542714520d2
Stephen Done <sdone@simplifile.com>
2016-11-22 08:20:24 -0700

    move to svg

--- src/app.js
+++ src/app.js
@@ -6,15 +6,20 @@ var scores = [
   { name: 'Emily', score: 88 }
 ];
 
-var update = d3.select('.chart')
+var bar = d3.select('.chart')
   .append('svg')
     .attr('width', 225)
     .attr('height', 300)
   .selectAll('rect')
   .data(scores)
   .enter()
-    .append('rect')
-    .attr('y', (d, i) => i * 33)
+    .append('g')
+    .attr('transform', (d, i) => 'translate(0, ' + i * 33 + ')');
+
+bar.append('rect')
     .style('width', d => d.score)
-    .text(d => d.name)
     .attr('class', 'bar')
+
+bar.append('text')
+  .attr('y', 20)
+  .text(d => d.name);
