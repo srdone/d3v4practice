62bd739e5cce707d63802df50b70f0940c69f673
Stephen Done <sdone@simplifile.com>
2016-11-22 08:52:50 -0700

    using .call to organize code

--- src/app.js
+++ src/app.js
@@ -6,6 +6,18 @@ var scores = [
   { name: 'Emily', score: 88 }
 ];
 
+function scaleBar (selection, scale) {
+  selection.style('transform', 'scaleX(' + scale + ')');
+}
+
+function setFill (selection, color) {
+  selection.style('fill', color);
+}
+
+function fade (selection, opacity) {
+  selection.style('fill-opacity', opacity);
+}
+
 var bar = d3.select('.chart')
   .append('svg')
     .attr('width', 225)
@@ -20,15 +32,21 @@ bar.append('rect')
     .style('width', d => d.score)
     .attr('class', 'bar')
     .on('mouseover', function (d, i, elements) {
-      d3.select(this).style('transform', 'scaleX(2)');
+      d3.select(this)
+        .call(scaleBar, 1.5)
+        .call(setFill, 'orange');
 
       d3.selectAll(elements)
         .filter(':not(:hover)')
-        .style('fill-opacity', 0.5);
+        .call(fade, 0.5);
     })
     .on('mouseout', function (d, i, elements) {
-      d3.select(this).style('transform', 'scaleX(1)');
-      d3.selectAll(elements).style('fill-opacity', 1);
+      d3.select(this)
+        .call(scaleBar, 1)
+        .call(setFill, 'lightgreen');
+
+      d3.selectAll(elements)
+        .call(fade, 1);
     });
 
 bar.append('text')
