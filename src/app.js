fad515af3e8f3cd607e21c11cb393415c19bb3f0
Stephen Done <sdone@simplifile.com>
2016-11-22 09:17:57 -0700

    adding axes

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-var margin = {top: 0, right: 0, bottom: 25, left: 25 };
+var margin = {top: 10, right: 0, bottom: 60, left: 40 };
 var width = 425 - margin.left - margin.right;
 var height = 625 - margin.top - margin.bottom;
 
@@ -14,3 +14,24 @@ svg.append('rect')
   .attr('height', height)
   .style('fill', 'lightblue')
   .style('stroke', 'green');
+
+var yScale = d3.scaleLinear()
+  .domain([0, 1])
+  .range([height, 0]);
+
+var yAxis = d3.axisLeft(yScale);
+svg.call(yAxis);
+
+var xScale = d3.scaleTime()
+  .domain([new Date(2016, 0, 1, 6), new Date(2016, 0, 1, 9)])
+  .range([0, width]);
+
+var xAxis = d3.axisBottom(xScale)
+  .ticks(5)
+  .tickSize(10)
+  .tickPadding(5);
+
+svg
+  .append('g')
+    .attr('transform', `translate(0, ${height})`)
+  .call(xAxis);
